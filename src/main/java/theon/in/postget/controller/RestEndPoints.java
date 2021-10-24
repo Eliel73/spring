3f9583196b1aa83965383cc9c8ed815cc11b2d1a
Eliel73 <eliel.pereira73@outlook.com>
2021-10-24 16:36:25 -0300

    now with comments!

--- src/main/java/theon/in/postget/controller/RestEndPoints.java
+++ src/main/java/theon/in/postget/controller/RestEndPoints.java
@@ -4,25 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-@RestController // It's used to
+@RestController // @RestController is an annotation that is used to define that the controller is providing REST services with JSON type response.
 public class RestEndPoints {
 
-    @Value("${default.course.name}")
+    @Value("${default.course.name}")  // @Value is an annotation that is used to set variable's value using a properties file.
     private String name;
 
     @Value("${default.course.chapterCount}")
     private int chapterCount;
 
-    @Autowired
+    @Autowired //@Autowired is an annotation that allows us to instantiate a class without necessarily using a setter or constructor, which makes it much easier to use the concept of Dependency Injection (DI).
     private CourseConfig config;
 
-    @RequestMapping("/course")
+    @RequestMapping("/course") //@RequestMapping is an annotation that is used to set the url path. By default, the method is GET.
     public Course getEndpoint(@RequestParam(name="name", defaultValue = "String boot", required = false) String name,
                               @RequestParam(name="chapterCount", defaultValue = "5", required = false) int chapterCount){
         return new Course(name, chapterCount);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/course")
+    @RequestMapping(method = RequestMethod.POST, value = "/course") //It's used to set the url path, although the method (POST, GET, DELETE, PUT) is specific, in this case POST
     public String postEndpoint(@RequestBody Course course){
         return String.format("You save: %s with %d chapters", config.getName(), config.getChapterCount());
     }
